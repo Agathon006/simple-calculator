ec113ffc63913d501127eaa974a9551af3c1186e
Agathon006 <agaton615@gmail.com>
2024-10-24 11:28:48 +0300

    feat: theme switcher logic

---
+++ src/scripts/features/switchTheme.js
@@ -0,0 +1,14 @@
+export default () => {
+  const switchBtn = document.querySelector("#theme-switch");
+  const wrapper = document.querySelector(".wrapper");
+
+  switchBtn.addEventListener("click", () => {
+    if (wrapper.classList.contains("dark-theme")) {
+      wrapper.classList.remove("dark-theme");
+      wrapper.classList.add("light-theme");
+    } else {
+      wrapper.classList.remove("light-theme");
+      wrapper.classList.add("dark-theme");
+    }
+  });
+};
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,2 +1,7 @@
 "use strict";
 
+import switchTheme from "./features/switchTheme.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+  switchTheme();
+});
